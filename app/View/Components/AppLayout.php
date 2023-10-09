d4170659786448eeea0d4ef3f07df85b619349fc
Hannah Eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2023-10-09 10:08:32 +1000

    wiretoast in app

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -5,8 +5,12 @@ namespace App\View\Components;
 use Illuminate\View\Component;
 use Illuminate\View\View;
 
+use Usernotnull\Toast\Concerns\WireToast; // Import the WireToast trait
+
 class AppLayout extends Component
 {
+    use WireToast; // Add the WireToast trait to your component
+
     /**
      * Get the view / contents that represents the component.
      */
